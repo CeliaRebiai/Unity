437b7e8aa41ec91c53f4f7cc2d08d17dc694c886
DESKTOP-TALTCMI\Celia <celiarebiai70@gmail.com>
2024-03-01 13:14:29 +0100

    no message

---
+++ My project/Assets/Scripts/MyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float maxTime = 5.6f;
+        Debug.Log("val" + maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
